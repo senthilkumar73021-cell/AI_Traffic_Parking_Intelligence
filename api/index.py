a4536b0422490b78e17e8ab1d7393d9aa8cb1562
senthilkumar73021-cell <senthilkumar73021@gmail.com>
2026-08-31 12:11:56 +0530

    Fix Vercel deployment

--- api/index.py
+++ api/index.py
@@ -1,1 +1,16 @@
-from web import app
+import sys
+import os
+
+# Add project root folder to Python path
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+from web import app, create_database
+
+# Create database when Vercel starts the function
+create_database()
